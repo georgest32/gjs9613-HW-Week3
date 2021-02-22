d49c0c48afe15d5f41a7c598361d7f5ef24eedb2
george <georgest32@gmail.com>
2021-02-22 07:42:30 -1000

    added unity project

---
+++ gjs9613-week3/Assets/Scripts/GameManager.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private int score;
+    public Text text;
+    const string DIR_LOGS = "/Logs";
+    const string FILE_SCORES = DIR_LOGS + "/highScore.txt";
+    const string FILE_ALL_SCORES = DIR_LOGS + "/allScores.csv";
+    string FILE_PATH_HIGH_SCORES;
+    string FILE_PATH_ALL_SCORES;
+    
+    public int Score
+    {
+        get { return score; }
+        set
+        {
+            score = value;
+
+            if (score > HighScore)
+            {
+                HighScore = score;
+            }
+
+            //Don't need to write all scores
+            
+            // string fileContents = "";
+            // if (File.Exists(FILE_PATH_ALL_SCORES))
+            // {
+            //     fileContents = File.ReadAllText(FILE_PATH_ALL_SCORES);
+            // }
+            //
+            // fileContents += score + ",";
+            // File.WriteAllText(FILE_PATH_ALL_SCORES, fileContents);
+        }
+    }
+    
+    int highScore = -1;
+
+    public int HighScore
+    {
+        get
+        {
+            if (highScore < 0)
+            {
+                if (File.Exists(FILE_PATH_HIGH_SCORES))
+                {
+                    string fileContents = File.ReadAllText(FILE_PATH_HIGH_SCORES);
+                    highScore = Int32.Parse(fileContents);
+                }
+                else
+                {
+                    highScore = 3;
+                }
+            }
+
+            return highScore;
+        }
+        set
+        {
+            Debug.Log("New High Score!");
+            highScore = value;
+            
+            if (!File.Exists(FILE_PATH_HIGH_SCORES))
+            {
+                Directory.CreateDirectory(Application.dataPath + DIR_LOGS);
+            }
+
+            File.WriteAllText(FILE_PATH_HIGH_SCORES, highScore + "");
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        FILE_PATH_HIGH_SCORES = Application.dataPath + FILE_SCORES;
+        FILE_PATH_ALL_SCORES  = Application.dataPath + FILE_ALL_SCORES;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "SCORE: " + score +
+                    "\nHIGH SCORE: " + HighScore;
+    }
+}
---
+++ gjs9613-week3/Assets/Scripts/PaddleController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    public GameObject paddleRight;
+    public GameObject paddleLeft;
+    public GameObject ball;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.F))
+        {
+            paddleLeft.transform.Rotate(new Vector3(0,0,45));
+        }
+        
+        if (Input.GetKey(KeyCode.J))
+        {
+            paddleRight.transform.Rotate(new Vector3(0,0,45));
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            ball.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 250);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            other.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 500);
+        }
+    }
+}
